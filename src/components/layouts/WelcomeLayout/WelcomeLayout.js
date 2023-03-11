9a6d90fc4608d93dc75b17ba686d6acc1cac15ab
HyeongjaeLee <tlwprosper@gmail.com>
2023-03-12 02:21:55 +0900

    Welcome 화면 완료

--- src/components/layouts/WelcomeLayout/WelcomeLayout.js
+++ src/components/layouts/WelcomeLayout/WelcomeLayout.js
@@ -1,17 +1,20 @@
 import { Spin } from "antd";
 import React, { useState } from "react";
 import Content from "./Content";
+import { Link } from "react-router-dom";
 
 const WelcomeLayout = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <Spin spinning={isLoading} tip="잠시만 기다려주세요..">
-      <div id="welcome-layout">
-        <Content>
-          {React.cloneElement(children, { setIsLoading })}
-        </Content>
-      </div>
+      <Link to="/main" style={{color: "inherit"}}>
+        <div id="welcome-layout">
+          <Content>
+            {React.cloneElement(children, { setIsLoading })}
+          </Content>
+        </div>
+      </Link>
     </Spin>
   );
 };
