46b9014f0bf90996c79e199234cf7ed7a44bb83d
yjleeplan <yjlee@chahoo.com>
2023-03-14 00:29:57 +0900

    feature: pc버전 및 404 페이지 추가

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,32 +3,60 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "../components/common/ScrollToTop";
 import WelcomeLayout from "../components/layouts/WelcomeLayout/WelcomeLayout";
 import MainLayout from "../components/layouts/MainLayout/MainLayout";
+import PagePC from "../components/common/PagePC";
+import Page404 from "../components/common/Page404";
 import Welcome from "../components/pages/Welcome/Welcome";
 import Main from "../components/pages/Main/Main";
 
 const Routes = () => {
+  const isMobile = () => {
+    const pc = 'win16|win32|win64|mac|macintel';
+    if (navigator.platform) {
+      if (pc.indexOf(navigator.platform.toLowerCase()) < 0) return true;
+      else return false;
+    }
+    else return true;
+  };
+
   return (
     <Router>
       <ScrollToTop />
       <Switch>
-        <Route
-          exact
-          path="/main"
-          render={(props) => (
-            <MainLayout>
-              <Main {...props} />
-            </MainLayout>
-          )}
-        />
-        <Route
-          exact
-          path="/"
-          render={(props) => (
-            <WelcomeLayout>
-              <Welcome {...props} />
-            </WelcomeLayout>
-          )}
-        />
+        {isMobile() ? (
+          <>
+            <Route
+              exact
+              path="/main"
+              render={(props) => (
+                <MainLayout>
+                  <Main {...props} />
+                </MainLayout>
+              )}
+            />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <WelcomeLayout>
+                  <Welcome {...props} />
+                </WelcomeLayout>
+              )}
+            />
+            <Route
+              exact
+              path="*"
+              render={(props) => (
+                <Page404 {...props} />
+              )}
+            />
+          </>
+        ) : (
+          <Route
+            render={(props) => (
+              <PagePC {...props} />
+            )}
+          />
+        )}
       </Switch>
     </Router>
   );
