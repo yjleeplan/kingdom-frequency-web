720634293ea7b0405533c6d6d84e4d48c1866ce5
yjleeplan <yjlee@chahoo.com>
2023-03-24 22:57:44 +0900

    fix: pc 화면 추가

--- src/components/common/PagePC.js
+++ src/components/common/PagePC.js
@@ -1,19 +1,37 @@
+import { Col, Row, Typography } from "antd";
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+
+const { Text } = Typography;
 
 const PagePC = () => {
   return (
     <>
-      <div className='on-pc-icon'>
-        <FontAwesomeIcon icon={faExclamationTriangle} />
+      <div id="welcome-layout">
+        <div id="content">
+          <Row className="project-year">
+            <Col span={24}>
+              2023
+            </Col>
+          </Row>
+          <Row className="project-sub-title">
+            <Col span={24}>
+              금광교회 결단 프로젝트
+            </Col>
+          </Row>
+          <Row className="project-title">
+            <Col span={24}>
+              <Text className='color-0'>K</Text>INGDOM
+              <br/>
+              <Text className='color-0'>F</Text>REQUEN<Text className='color-0'>C</Text>Y
+            </Col>
+          </Row>
+          <Row className="pc-comment">
+            <Col span={24}>
+              모바일로 접속 해주세요.
+            </Col>
+          </Row>
+        </div>
       </div>
-      <span className='on-pc-status-code'>
-        403
-      </span>
-      <span className='on-pc-status-message'>
-        모바일 전용웹 입니다. 모바일로 접속 해주세요.
-      </span>
     </>
   );
 };
